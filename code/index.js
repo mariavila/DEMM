3f14da0c605c9977de3d62e7c593bb8960b17d2b
mariavila <mariava.1213@gmail.com>
2017-03-05 08:08:31 +0100

    wheretogo

--- code/index.js
+++ code/index.js
@@ -29,7 +29,8 @@ var injured = {}; //all injured
 var people = {}; //userId of all users
 
 
-var injuredCOUNT = 1; //CANVIAR A 0!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+var injuredCOUNT = 3; //CANVIAR A 0!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 //Client server
 io.on('connection', function(socket){
@@ -79,32 +80,21 @@ io.on('connection', function(socket){
     console.log([medicalAid, injured]);
   });
 
-  socket.on('disconnect', function(){
-    delete people[socket.id];
-    if(socket.id in injured){
-      delete medicalAid[socket.id];
-      injuredCOUNT--;
-    }
-  });
-});
+  socket.on('askWheretogo', function(){
+    if(injuredCOUNT>0){
+      injured["_12345"] = {motionless: 0, latitude: 41.3918234, longitude: 2.1155787};
+      injured["_12346"] = {motionless: 0, latitude: 41.386895, longitude:  2.162351};
+      injured["_12347"] = {motionless: 0, latitude: 41.393833,  longitude: 2.197119};
 
-
-
-function mainloop() {
-  if(injuredCOUNT>0){
-    injured["_12345"] = {motionless: 0, latitude: 41.3918234, longitude: 2.1155787};
-    injured["_12346"] = {motionless: 0, latitude: 41.386895, longitude:  2.162351};
-    injured["_12347"] = {motionless: 0, latitude: 41.393833,  longitude: 2.197119};
-
-    medicalAid["_0987654"] = {latitude: 41.394605, longitude: 2.150028};
-    medicalAid["_0987653"] = {latitude: 41.405804,  longitude: 2.188877};
-    medicalAid["_0987652"] = {latitude: 41.410492,  longitude: 2.158905};
+      medicalAid["_0987654"] = {latitude: 41.394605, longitude: 2.150028};
+      medicalAid["_0987653"] = {latitude: 41.405804,  longitude: 2.188877};
+      medicalAid["_0987652"] = {latitude: 41.410492,  longitude: 2.158905};
     
-    var state ={"healers" : medicalAid,"injured": injured};
+      var state ={"healers" : medicalAid,"injured": injured};
 
     //Call calculate routes function
     var routes = planner.solve(state);
-    //console.log(state);
+
     //Send routes to clients
     for(var socketId in routes){
       console.log(socketId);
@@ -117,6 +107,14 @@ function mainloop() {
       }
     }
   }
-}
+  })
 
-setInterval(mainloop,10000);
+  socket.on('disconnect', function(){
+    delete people[socket.id];
+    if(socket.id in injured){
+      delete medicalAid[socket.id];
+      injuredCOUNT--;
+    }
+  });
+
+});
