a5ad3db32f2b1f6ed4c7edd74e48cfd57b14376b
marcmillet <marc170995@gmail.com>
2017-03-05 03:04:28 +0100

    Code cleaning

--- code/public/js/app.js
+++ code/public/js/app.js
@@ -9,15 +9,7 @@ $(document).ready(function() {
 
 
 //Init map
-  function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 13
-        });
-        var marker = new google.maps.Marker({
-          position: {lat: -34.397, lng: 150.644},
-          map: map
-        });
+  function initGeo() {
 
         // Try HTML5 geolocation.
         if (navigator.geolocation) {
@@ -26,26 +18,9 @@ $(document).ready(function() {
               lat: position.coords.latitude,
               lng: position.coords.longitude
             };
-
-            map.setCenter(pos);
-            marker.setPosition(pos)
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
+          })
         }
-      }
-
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-      }
-
-    google.maps.event.addDomListener(window, 'load', initMap);
+      };
 
 
 //Hide and show divs
@@ -53,7 +28,7 @@ $(document).ready(function() {
 
   var socket = io();
 
-  initMap();
+  initGeo();
 
   user["latitude"] = pos["lat"];
   user["longitude"] = pos["lng"];
